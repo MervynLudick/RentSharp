32ce571a34d8f6c72fcfa3f4ebd32400c845b00c
Mervyn Ludick <mervyn@specsavers.co.za>
2023-05-06 21:31:03 +0200

    Add Blazor and Test Page for Clients

---
+++ RentSharp.Web/Client/Client/CustomerListModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RentSharp.Web.Client.Client
+{
+    public class CustomerListModel
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Cellphone { get; set; }
+        public string IdNumber { get; set; }
+    }
+
+}
---
+++ RentSharp.Web/Client/Client/MockCustomerData.cs
@@ -0,0 +1,43 @@
+﻿namespace RentSharp.Web.Client.Client
+{
+    using System.Collections.Generic;
+
+    public static class MockCustomerData
+    {
+        public static List<CustomerListModel> GetCustomers()
+        {
+            return new List<CustomerListModel>
+            {
+                new CustomerListModel()
+                {
+                    FirstName = "John",
+                    Surname = "Doe",
+                    Cellphone = "555-123-4567",
+                    IdNumber = "1234567890"
+                },
+                new CustomerListModel()
+                {
+                    FirstName = "Jane",
+                    Surname = "Doe",
+                    Cellphone = "555-987-6543",
+                    IdNumber = "0987654321"
+                },
+                new CustomerListModel()
+                {
+                    FirstName = "Alice",
+                    Surname = "Smith",
+                    Cellphone = "555-123-9876",
+                    IdNumber = "1029384756"
+                },
+                new CustomerListModel()
+                {
+                    FirstName = "Bob",
+                    Surname = "Johnson",
+                    Cellphone = "555-654-3210",
+                    IdNumber = "5647382910"
+                }
+            };
+        }
+    }
+
+}
