a0b72842e150ef23d45b945e3f5c84a9a822e835
Mervyn Ludick <mervyn@specsavers.co.za>
2023-05-07 16:09:05 +0200

    UI Changes

---
+++ RentSharp.Web/Shared/ViewModels/CreateCustomerViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace RentSharp.Web.Shared.ViewModels
+{
+    public class CreateCustomerViewModel
+    {
+        [Required]
+        public string? FirstName { get; set; }
+        [Required]
+        public string? Surname { get; set; }
+        [Required]
+        public string? Cellphone { get; set; }
+        [EmailAddress]
+        public string? EmailAddress { get; set; }
+        [Required]
+        [MaxLength(13)]
+        [RegularExpression(@"^\d+$")]
+        public string? IdNumber { get; set; }
+        [Required]
+        public string? AddressLine1 { get; set; }
+        public string? AddressLine2 { get; set; }
+        [Required]
+        public string? Suburb { get; set; }
+        public string? City { get; set; }
+        public string? PostalCode { get; set; }
+    }
+}
